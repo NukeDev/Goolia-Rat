d1ac4be151df9407ed3d89c4161c57f4086f39a9
NukeDev <info@cloudwave.it>
2020-04-12 19:11:00 +0200

    Updated Client connection
    * Managed exit on server disconnect
    + Client retry to connects since is connected
    * Minor fixes

--- client/client.go
+++ client/client.go
@@ -14,6 +14,7 @@ import (
 	"google.golang.org/grpc"
 )
 
+//Client local struct
 type Client struct {
 	ID        string
 	IPAddress string
@@ -24,108 +25,125 @@ var commandWaitingList []string
 
 func main() {
 
-	// dail server
-	conn, err := grpc.Dial(":50005", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("can not connect with server %v", err)
+	for {
+		ClientProcess()
 	}
 
-	// create stream
-	localClient.Generate()
-	client := pb.NewComClient(conn)
-	stream, err := client.HandleCommands(context.Background())
-	if err != nil {
-		log.Fatalf("openn stream error %v", err)
-	}
-
-	ctx := stream.Context()
-	done := make(chan bool)
-
-	// first goroutine sends random increasing numbers to stream
-	// and closes int after 10 iterations
-	go func() {
-		for {
-
-			// generate random nummber and send it to stream
-			req := pb.Request{ClientID: localClient.ID, ClientIPAddress: localClient.IPAddress, Command: "ping", Data: nil}
-			if err := stream.Send(&req); err != nil {
-				log.Fatalf("can not send ping to master server %v", err)
-			}
-			log.Printf("Sending ping to master server")
-			time.Sleep(time.Second * 10)
-		}
-
-	}()
-
-	// second goroutine receives data from stream
-	// and saves result in max variable
-	//
-	// if stream is finished it closes done channel
-	go func() {
-		for {
-			resp, err := stream.Recv()
-			if err == io.EOF {
-				return
-			}
-			if err != nil {
-				log.Fatalf("can not receive %v", err)
-			}
-
-			switch resp.Command {
-			case "ping":
-				{
-					log.Println("Ping OK!")
-				}
-			case "osinfo":
-				{
-
-					commandWaitingList = append(commandWaitingList, "osinfo")
-					info, err := osinfo.GetOsInfo()
-					if err != nil {
-						continue
-					}
-
-					b, err := json.Marshal(info)
-
-					if err != nil {
-						log.Fatalf("%v", err)
-					}
-
-					req := pb.Request{ClientID: localClient.ID, ClientIPAddress: localClient.IPAddress, Command: "osinfo", Data: b}
-
-					if err := stream.Send(&req); err != nil {
-						log.Fatalf("can not send OSINFO to master server %v", err)
-					}
-					log.Printf("Sending OSINFO to master server")
-
-				}
-			case "not-found":
-			default:
-				{
-					log.Println("No command found as %s", resp.Command)
-				}
-			}
-		}
-	}()
-
-	// third goroutine closes done channel
-	// if context is done
-	go func() {
-		<-ctx.Done()
-		if err := ctx.Err(); err != nil {
-			log.Println(err)
-		}
-		close(done)
-	}()
-
-	<-done
-	log.Printf("finished with")
 }
 
+func ClientProcess() {
+	for {
+		time.Sleep(time.Second * 5)
+		// dail server
+		conn, err := grpc.Dial(":50005", grpc.WithInsecure(), grpc.WithTimeout(time.Second*10000))
+		if err != nil {
+			log.Printf("Can't connect to Master Server!! - %v", err)
+			continue
+		}
+
+		//generate client data
+		localClient.Generate()
+		// create stream
+		client := pb.NewComClient(conn)
+
+		stream, err := client.HandleCommands(context.Background())
+		if err != nil {
+			log.Printf("There was an error while opening the stream - %v", err)
+			continue
+		}
+		ctx := stream.Context()
+		done := make(chan bool)
+
+		// first goroutine sends ping requests to master servers to wait commands
+		go func() {
+			for {
+
+				req := pb.Request{ClientID: localClient.ID, ClientIPAddress: localClient.IPAddress, Command: "ping", Data: nil}
+				if err := stream.Send(&req); err != nil {
+					log.Printf("Can't send PING request to Master Server - %v", err)
+					log.Println("Reloading connection...")
+					break
+				}
+				log.Printf("PING request sent to Master Server")
+				time.Sleep(time.Second * 10)
+			}
+
+		}()
+
+		// second goroutine receives data from stream
+		// if stream is finished it closes done channel
+		go func() {
+			for {
+				resp, err := stream.Recv()
+				if err == io.EOF {
+					log.Printf("%v", err)
+					log.Println("Reloading connection...")
+					break
+				}
+				if err != nil {
+					log.Printf("Can't receive on opened channel %v", err)
+					log.Println("Reloading connection...")
+					break
+				}
+
+				switch resp.Command {
+				case "ping":
+					{
+						log.Println("Ping OK!")
+					}
+				case "osinfo":
+					{
+						info, err := osinfo.GetOsInfo()
+						if err != nil {
+							log.Printf("%v", err)
+						}
+
+						b, err := json.Marshal(info)
+
+						if err != nil {
+							log.Printf("%v", err)
+
+						}
+
+						req := pb.Request{ClientID: localClient.ID, ClientIPAddress: localClient.IPAddress, Command: "osinfo", Data: b}
+
+						if err := stream.Send(&req); err != nil {
+							log.Printf("can not send OSINFO to master server %v", err)
+							break
+						}
+						log.Printf("Sending OSINFO to master server")
+
+					}
+				case "not-found":
+				default:
+					{
+						log.Printf("No command found as %s", resp.Command)
+					}
+				}
+			}
+		}()
+
+		// third goroutine closes done channel
+		// if context is done
+		go func() {
+			<-ctx.Done()
+			if err := ctx.Err(); err != nil {
+				log.Println(err)
+			}
+			close(done)
+		}()
+
+		<-done
+		log.Printf("Connection finished...")
+		log.Println("Reloading connection...")
+	}
+}
+
+//Generate Client Unique ID and Gets Public IPAddress
 func (cl *Client) Generate() {
 	id, err := machineid.ProtectedID("GooliaClient")
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 	cl.ID = id
 
